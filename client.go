86e7b336874676471bdc76cdb12d102f19cd033d
走哪都有风 <956035125@qq.com>
2017-10-23 11:37:10 +0800

    1

--- client.go
+++ client.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"flag"
-	"net"
 	"fmt"
+	"net"
 	"os"
 	"strconv"
+	"sync"
 )
 
 var host = flag.String("host", "localhost", "host")
@@ -21,38 +22,39 @@ func main() {
 	defer conn.Close()
 	fmt.Println("Connecting to " + *host + ":" + *port)
 
-	done := make(chan string)
+	// done := make(chan string)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go handleWrite(conn, &wg)
+	go handleRead(conn, &wg)
 
-	go handleWrite(conn,done)
-	go handleRead(conn,done)
+	wg.Wait()
 
-
-
-
-
-	fmt.Println(<-done)
-	fmt.Println(<-done)
+	// fmt.Println(<-done)
+	// fmt.Println(<-done)
 }
 
-func handleWrite(conn net.Conn, done chan string) {
+func handleWrite(conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for i := 10; i > 0; i-- {
 		_, e := conn.Write([]byte("hello " + strconv.Itoa(i) + "\r\n"))
 
 		if e != nil {
-			fmt.Println("Error to send message because of ",e.Error())
+			fmt.Println("Error to send message because of ", e.Error())
 			break
 		}
 	}
-	done <- "Sent"
+	// done <- "Sent"
 }
 
-func handleRead(conn net.Conn, done chan string) {
-	buf := make([]byte,1024)
+func handleRead(conn net.Conn, wg *sync.WaitGroup) {
+	wg.Done()
+	buf := make([]byte, 1024)
 	reqLen, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Error to read message because of ",err)
+		fmt.Println("Error to read message because of ", err)
 		return
 	}
 	fmt.Println(string(buf[:reqLen-1]))
-	done <- "Read"
+	// done <- "Read"
 }
